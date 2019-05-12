0ae45a7040600ddfcc3f347416e883c31b492338
Stefan Zima <stefanzima@Stefans-MacBook-Pro.local>
2019-05-12 18:34:34 +0200

    Random bird assignment works

--- src/app/services/get-random-bird.service.ts
+++ src/app/services/get-random-bird.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+export interface Bird {
+  name: string;
+  images: string[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class GetRandomBirdService {
 
+  private birds: Bird[] = [
+    {
+      name: 'struhio-camelus',
+      images: [
+        'struhio-camelus/1.jpg',
+        'struhio-camelus/2.jpg',
+        'struhio-camelus/3.jpg',
+        'struhio-camelus/4.jpg',
+      ]
+    }
+  ]
+
   constructor() { }
+
+  public giveMeBird() {
+    return this.birds[this.getRndInteger(0, this.birds.length - 1)]
+  }
+
+  public getRndInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 }
